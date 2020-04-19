1be3f5d388e344c90b86b710011b0f5b0ed106d9
Alfred Carro <alfreddannvincentfcarro@gmail.com>
2020-04-20 00:42:02 +0800

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import "./App.css";
+
+import React from "react";
+
+import logo from "./logo.svg";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+      <h3>This is the Start of Your Portfolio :)</h3>
+      <div>
+        <h1>Alfred Dann Vincent F. Carro</h1>
+      </div>
+      <div>
+        <h2>
+          April 20, 2020
+        </h2>
+      </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
