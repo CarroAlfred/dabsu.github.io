69a3f56c922d0148e6e332f5f822bef2c7cca1c0
dabsu <alfreddannvincentfcarro@gmail.com>
2020-04-20 20:49:50 +0800

    Updates

---
+++ precache-manifest.04b774a02a316d1ee3c92611d9a1939d.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a01a5d0e2b7c1989a9606248f3b97ef",
+    "url": "/index.html"
+  },
+  {
+    "revision": "28892b4c0161d4469a0b",
+    "url": "/static/css/main.8ebe3114.chunk.css"
+  },
+  {
+    "revision": "b97366d71dd46964430a",
+    "url": "/static/js/2.942a928c.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.942a928c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28892b4c0161d4469a0b",
+    "url": "/static/js/main.92d4d26e.chunk.js"
+  },
+  {
+    "revision": "5284eec07972ace79c95",
+    "url": "/static/js/runtime-main.ceaf4b5f.js"
+  },
+  {
+    "revision": "bafd78da573962d22a817cf22678c8dc",
+    "url": "/static/media/KaushanScript-Regular.bafd78da.ttf"
+  },
+  {
+    "revision": "8ffdd063a0a1516516fc5d724c962ffb",
+    "url": "/static/media/Neuton-Bold.8ffdd063.ttf"
+  },
+  {
+    "revision": "a789023b7e36994005c80ef9ea967d2c",
+    "url": "/static/media/Neuton-ExtraBold.a789023b.ttf"
+  },
+  {
+    "revision": "e2b7bf63173101d603659491a2446c6e",
+    "url": "/static/media/Neuton-ExtraLight.e2b7bf63.ttf"
+  },
+  {
+    "revision": "75c153ac5c7f0b7e9f5e9b4c13465ee4",
+    "url": "/static/media/Neuton-Light.75c153ac.ttf"
+  },
+  {
+    "revision": "165ad7183be9563e73c9ef7d26b3dadb",
+    "url": "/static/media/Neuton-Regular.165ad718.ttf"
+  },
+  {
+    "revision": "2a907025da59acf289146e7ff3e43633",
+    "url": "/static/media/avatar-sm.2a907025.png"
+  },
+  {
+    "revision": "8ad1d96260182feac99d096f06f40f1d",
+    "url": "/static/media/avatar.8ad1d962.png"
+  }
+]);
